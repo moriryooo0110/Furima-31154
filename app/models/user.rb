160fcc69197c69cb0b9f1143458a88734a4932f5
moriryooo0110 <the.obvious_simple@icloud.com>
2020-11-04 15:30:41 +0900

    ログイン、ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validate :password_complexity
+  def password_complexity
+# Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+  return if password.blank? || password =~ /^(?=.*?[a-z])(?=.*?[0-9]).{8,70}$/
+      errors.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
   validates :nickname, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
