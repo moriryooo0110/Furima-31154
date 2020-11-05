1c6865ff756a102c7b3bd313f15b5b3fd27424f0
moriryooo0110 <the.obvious_simple@icloud.com>
2020-11-05 19:40:02 +0900

    新規出品投稿機能のビューファイル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
@@ -7,11 +8,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-  
+   @item = Item.new(item_params)
+   if @item.save
+    redirect_to root_path
+   else
+    render :new
   end    
+ end
+
+ private
+
+  def item_params
+  params.require(:item).permit(:product_name,:description_of_item, :category_id, :days_to_ship_id, :product_status_id, :shipment_source_id, :shipping_fee_burden_id, :price).merge(user_id: current_user.id)
+  end 
+
 end
-
-private
-
-def item_params
-  params.require(:item).permit(:)
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,27 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to :category
+  belongs_to :days_to_ship
+  belongs_to :product_status
+  belongs_to :shipment_source
+  belongs_to :shipping_fee_burden
   has_one_attached :image
+
+
+  with_options presence: true do
+    validates :product_name
+    validates :description_of_item
+    validates :price
+    validates :content, unless: :wasattached?
+  end
+
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :days_to_ship_id
+    validates :product_status
+    validates :shipment_source
+    validates :shipping_fee_burden
+  end
 end
