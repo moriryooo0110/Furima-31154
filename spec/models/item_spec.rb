35c6203b56d2a49a68d9b31e060c4a083d9d3404
moriryooo0110 <the.obvious_simple@icloud.com>
2020-11-07 13:23:56 +0900

    コメントアウトの削除、単体テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Item, type: :model do
      @item = FactoryBot.build(:item)
    end
 
+   it "全ての項目が存在すれば登録出来る事" do
+    expect(@item).to be_valid
+  end
+
    it "商品画像を1枚つけることが必須であること" do
     @item.image = nil
     @item.valid?
@@ -30,18 +34,35 @@ RSpec.describe Item, type: :model do
      expect(@item.errors.full_messages).to include("Category is not a number")
    end
 
+   it "選択カテゴリーの情報が(--)だと登録出来ない事" do
+    @item.category_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Category must be other than 1")
+  end
+
    it "商品の状態についての情報が必須であること" do
      @item.product_status_id = nil
      @item.valid?
      expect(@item.errors.full_messages).to include("Product status is not a number")
-
    end
+
+   it "選択した商品状態の情報が(--)だと登録出来ない事" do
+    @item.product_status_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Product status must be other than 1")
+  end
 
    it "配送料の負担についての情報が必須であること" do
     @item.shipping_fee_burden_id = nil
     @item.valid?
     expect(@item.errors.full_messages).to include("Shipping fee burden is not a number")
    end
+
+   it "選択した配送料の負担についての情報が(--)だと登録出来ない事" do
+    @item.shipping_fee_burden_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Shipping fee burden must be other than 1")
+  end
 
 
    it "発送元の地域についての情報が必須であること" do
@@ -50,10 +71,22 @@ RSpec.describe Item, type: :model do
      expect(@item.errors.full_messages).to include("Shipment source is not a number")
    end
 
+   it "選択した発送元の地域についての情報が(--)だと登録出来ない事" do
+     @item.shipment_source_id = 1
+     @item.valid?
+     expect(@item.errors.full_messages).to include("Shipment source must be other than 1")
+   end
+
    it "発送までの日数についての情報が必須であること" do
-    @item.days_to_ship_id = nil
+     @item.days_to_ship_id = nil
      @item.valid?
      expect(@item.errors.full_messages).to include("Days to ship is not a number")
+   end
+
+   it "発送までの日数についての情報が(--)だと登録出来ない事" do
+    @item.days_to_ship_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Days to ship must be other than 1")
    end
 
    it "価格についての情報が必須であること" do
