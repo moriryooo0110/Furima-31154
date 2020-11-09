e4d48da265141525f38ace97668157015a952d02
moriryooo0110 <the.obvious_simple@icloud.com>
2020-11-09 19:55:24 +0900

    【修正】商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-      redirect_to root_path
+    if current_user.id == @item.user_id
+      @item.destroy
+      redirect_to root_path 
     end
   end
 
