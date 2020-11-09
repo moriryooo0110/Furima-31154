6816d264b455bb8fda8f4c0e6b568f6b3bca18e0
moriryooo0110 <the.obvious_simple@icloud.com>
2020-11-09 19:09:18 +0900

    【修正】before_actionにてitemsコントローラー定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -19,28 +20,28 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
     @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
-    redirect_to item_path
+    if @item.update(item_params)
+      redirect_to item_path
     else
-    render :edit
+      render :edit
     end
   end
-
 
   private
 
   def item_params
     params.require(:item).permit(:image, :product_name, :description_of_item, :category_id, :days_to_ship_id, :product_status_id, :shipment_source_id, :shipping_fee_burden_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
